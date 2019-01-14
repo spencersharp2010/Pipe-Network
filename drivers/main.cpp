50d59afad4e8b3ab6eebf22eb402105d39b0abd3
ge73ver <ge73ver@mytum.de>
2019-01-14 20:55:47 +0100

    updated main

--- drivers/main.cpp
+++ drivers/main.cpp
@@ -11,7 +11,6 @@ int main()
 	cie::pipenetwork::PipeNetwork provided_pipe_system("input.txt");
 	provided_pipe_system.print_input_data();
 	provided_pipe_system.computeFluxes();
-	//std::vector<double> solution(provided_pipe_system.numberofTubes);
 	//solution = provided_pipe_system.computeFluxes();
 	//for (int i = 0; i < provided_pipe_system.numberofTubes; ++i)
 	//{
