74fcfb070183e8bc803cddb185149e58dd414080
ge73ver <ge73ver@mytum.de>
2019-01-22 11:55:19 +0200

    cleaning up code

--- drivers/main.cpp
+++ drivers/main.cpp
@@ -8,22 +8,17 @@ int main()
 {
 	std::cout << "pipe network problem" << std::endl;
 
-	cie::pipenetwork::PipeNetwork provided_pipe_system("input.txt");
+	cie::pipenetwork::PipeNetwork provided_pipe_system("pipedata.txt");
 	provided_pipe_system.print_input_data();
-	std::vector<double> fluxes(provided_pipe_system.numberofTubes());
-	fluxes = provided_pipe_system.computeFluxes();
-	std::cout << "solution below" << std::endl;
-	for (int i = 0; i < provided_pipe_system.numberofTubes(); ++i)
-	{
-		std::cout << fluxes[i] << std::endl;
-	}
-	//provided_pipe_system.print_solution();
-	//solution = provided_pipe_system.computeFluxes();
-	//for (int i = 0; i < provided_pipe_system.numberofTubes; ++i)
+	//std::vector<double> fluxes(provided_pipe_system.numberofTubes());
+	//fluxes = provided_pipe_system.computeFluxes();
+	//std::cout << "solution below" << std::endl;
+	//for (int i = 0; i < provided_pipe_system.numberofTubes(); ++i)
 	//{
-	//	std::cout << "---------------SOLUTION BELOW----------------" << std::endl;
-	//	std::cout << solution[i] << std::endl;
+	//	std::cout << fluxes[i] << std::endl;
 	//}
-	
+
+	provided_pipe_system.print_solution();
+		
 	return 0;
 }
